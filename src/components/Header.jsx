65e6f6078b566522207812f33bdb619f46bb78f2
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-11-11 09:10:58 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}}>
+        
+        
+    </div>
+  )
+}
+
+export default Header
