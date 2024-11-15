27056523912c81f36aa5f773f4e7248f951cbce9
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-11-15 09:16:31 +0530

    final done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Testimonails from './components/Testimonails'
 import Contact from './components/Contact'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Projects/>
       <Testimonails/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
